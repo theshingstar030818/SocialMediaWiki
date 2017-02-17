0b556a843b095a627465dbca8bb2d25dd3c4e523
tanzeelrana <tanzeelrana@live.com>
2017-02-17 17:51:28 -0500

    initialized a new spring mvc web project

---
+++ src/main/java/sudopkill/SocialMediaWikiApplication.java
@@ -0,0 +1,12 @@
+package sudopkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaWikiApplication.class, args);
+	}
+}
