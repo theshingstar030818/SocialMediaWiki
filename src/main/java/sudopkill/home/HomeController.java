caffb99d51ffad59d97b8a5020e25759f36def57
tanzeelrana <tanzeelrana@cmail.carleton.ca>
2017-03-05 02:50:53 -0500

    bug fix for header home module value active

--- src/main/java/sudopkill/home/HomeController.java
+++ src/main/java/sudopkill/home/HomeController.java
@@ -2,6 +2,7 @@ package sudopkill.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.security.Principal;
 
@@ -11,6 +12,11 @@ import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    @ModelAttribute("module")
+    String module() {
+        return "home";
+    }
 
     @GetMapping(value = {"/","/home"})
     String index(Principal principal) {
