c70c3a8e335286837c94909605e3da303e5aa38e
tanzeelrana <tanzeelrana@cmail.carleton.ca>
2017-03-04 19:44:16 -0500

    added home page

---
+++ src/main/java/sudopkill/home/HomeController.java
@@ -0,0 +1,19 @@
+package sudopkill.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+/**
+ * Created by tanzeelrana on 3/4/2017.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    String index(Principal principal) {
+        System.out.println("principal : " + principal);
+        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+    }
+}
