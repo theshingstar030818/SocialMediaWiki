6e67cc2267b9cc229c21df96109ed3ed2cd06b1f
tanzeelrana <tanzeelrana@cmail.carleton.ca>
2017-03-05 04:44:48 -0500

    OAuth with facebook and github complete and bug free, need to create a user service that will now store the current user, also need to make User class should be able to work with all the different Authorization Server providers. Facebook --> principal.getName() returns the userID where as github's principal.getName() returns the username

--- src/main/java/sudopkill/home/HomeController.java
+++ src/main/java/sudopkill/home/HomeController.java
@@ -1,10 +1,12 @@
 package sudopkill.home;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by tanzeelrana on 3/4/2017.
@@ -18,9 +20,19 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping(value = {"/","/home"})
+    @RequestMapping({"/","/home", "/user", "/me"})
     String index(Principal principal) {
-        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
-    }
+        System.out.println("principal :  " + principal);
+        if(principal != null){
 
+            Map<String, String> map = new LinkedHashMap<>();
+            map.put("name", principal.getName());
+
+            System.out.println("user name :  " + principal.getName());
+
+            return "home/homeSignedIn";
+        }else{
+            return "home/homeNotSignedIn";
+        }
+    }
 }
